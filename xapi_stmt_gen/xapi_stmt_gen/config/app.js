3838ad23638d113b9967787382c40baea2b54d11
hiroki-sawano <hiroki.sawano.2512@gmail.com>
2022-01-31 17:32:58 +0900

    Format config

--- xapi_stmt_gen/xapi_stmt_gen/config/app.js
+++ xapi_stmt_gen/xapi_stmt_gen/config/app.js
@@ -2,38 +2,38 @@ const config = {
   url: 'http://learninglocker',
   limit: 500,
   chunkSize: 100,
-  db:{
+  db: {
     // LMS database connection settings
-    lms:{
-      host:'moodle-docker_db_1',
-      port:5432,
-      database:'moodle',
-      username:'moodleuser',
-      password:'m@0dl3ing',
-      prefix:'mdl_'
+    lms: {
+      host: 'moodle-docker_db_1',
+      port: 5432,
+      database: 'moodle',
+      username: 'moodleuser',
+      password: 'm@0dl3ing',
+      prefix: 'mdl_'
     },
-    la:{
-      host:'learning_analytics_db',
-      port:5432,
-      database:'learning_analytics',
-      username:'learning_analytics',
-      password:'learning_analytics',
+    la: {
+      host: 'learning_analytics_db',
+      port: 5432,
+      database: 'learning_analytics',
+      username: 'learning_analytics',
+      password: 'learning_analytics',
     }
   },
-  LRS:{
+  LRS: {
     // LRS client with 'Overall Scopes: API All' checked
-    client:{
-      key:'',
-      secret:''
+    client: {
+      key: '',
+      secret: ''
     },
     // Store statements in each LRS with title matching actor's ePPN scope
-    ePPNScoped:false,
+    ePPNScoped: false,
   },
-  category:{
-    id:'http://moodle.org',
-    definition:{
-      type:'http://id.tincanapi.com/activitytype/source',
-      name:'Moodle',
+  category: {
+    id: 'http://moodle.org',
+    definition: {
+      type: 'http://id.tincanapi.com/activitytype/source',
+      name: 'Moodle',
       description:
         'Moodle is a open source learning platform designed to ' +
         'provide educators, administrators and learners with ' +
@@ -41,8 +41,8 @@ const config = {
         'personalized learning environments.'
     }
   },
-  platform:'Moodle',
-  language:'en',
+  platform: 'Moodle',
+  language: 'en',
   homepage: 'http://localhost:8000'
 };
 module.exports = config;
