13583444fe1b217876861647d8a8942484fbe335
hiroki-sawano <hiroki.sawano.2512@gmail.com>
2023-06-21 16:44:34 +0900

    Rename xapi containers

--- chibichilo/run.sh
+++ chibichilo/run.sh
@@ -12,7 +12,7 @@ fi
 if [ -f "$path_to_log" ] ; then
   cat "$path_to_log" | \
     docker run -i -e LMS_DOMAIN=${lms_domain} ${dir_name}_log_processor | \
-    docker exec -i xapi_video_logs sh -c "cat - > /app/videojs.csv; npm start"
+    docker exec -i chibichilo-xapi sh -c "cat - > /app/videojs.csv; npm start"
   cat "$path_to_log" | \
     docker run -i -e LMS_DOMAIN=${lms_domain} ${dir_name}_log_processor | \
     docker exec -i caliper_video_logs sh -c \
