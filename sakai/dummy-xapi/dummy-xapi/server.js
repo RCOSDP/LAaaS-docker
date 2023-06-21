efed6b23a86711a84cd78ea154135321f048b7a5
hiroki-sawano <hiroki.sawano.2512@gmail.com>
2023-06-21 17:05:01 +0900

    Remove sakai

--- sakai/dummy-xapi/dummy-xapi/server.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const generator = require('./generator');
-const log4js = require('log4js');
-const logger = log4js.getLogger();
-
-generator().then(() => {
-}).catch((err) => {
-  logger.error(err);
-});
