376c18161c450d659410606f0d7fcc8ebaaf502a
hiroki-sawano <hiroki.sawano.2512@gmail.com>
2023-06-23 13:10:16 +0900

    Add container argument

--- chibichilo/run.sh
+++ chibichilo/run.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-path_to_log=$1
-lms_domain=$2
-
-if [ $# != 2 ] ; then
-  echo "Usage: run.sh <path_to_log> <lms_domain>"
+if [ $# != 3 ] || ([ $1 != "xapi" ] && [ $1 != "caliper" ]); then
+  echo "Usage: run.sh [xapi|caliper] <PATH_TO_LOG> <LMS_DOMAIN>"
   exit 1
 fi
 
-if [ -f "$path_to_log" ] ; then
-  cat "$path_to_log" | \
-    docker compose run -T -e LMS_DOMAIN=${lms_domain} log-processor | \
-    docker exec -i chibichilo-xapi sh -c "cat - > /app/videojs.csv; npm start"
-  cat "$path_to_log" | \
-    docker compose run -T -e LMS_DOMAIN=${lms_domain} log-processor | \
-    docker exec -i chibichilo-caliper sh -c \
-      "cat | python3 log_processor_for_caliper.py; php app/run.php /videojs.csv"
+CONTAINER=chibichilo-$1
+if [ $1 = "xapi" ]; then
+  SCRIPT="cat - > /app/videojs.csv; npm start"
+elif [ $1 = "caliper" ]; then
+  SCRIPT="cat | python3 log_processor_for_caliper.py; php app/run.php /videojs.csv"
+fi
+PATH_TO_LOG=$2
+LMS_DOMAIN=$3
+
+if [ -f "$PATH_TO_LOG" ] ; then
+  cat "$PATH_TO_LOG" | \
+    docker compose run -T -e LMS_DOMAIN=${LMS_DOMAIN} log-processor | \
+    docker exec -i ${CONTAINER} sh -c "${SCRIPT}"
 fi
